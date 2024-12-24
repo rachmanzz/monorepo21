7f4b9847c5066771fdd0683e68a100ce83462f57
Muhammad Abdurrahman <rachman.sd@gmail.com>
2024-12-24 13:32:30 +0700

    fix: install and add cors

--- apps/backend/core/app.ts
+++ apps/backend/core/app.ts
@@ -3,11 +3,14 @@ dotenv.config();
 
 import express from "express"
 import useRoutes from "../routes/userRoutes"
+import cors from "cors"
 
 
 const app = express()
 
 const port = parseInt(process.env.PORT ?? "5000")
+
+app.use(cors());
 
 
 app.use(express.json());
